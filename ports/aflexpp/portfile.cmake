38572b7b61fde0dfd649301ce5e774c15d8293c6
Mahder Gebremedhin <mahdual@gmail.com>
2021-11-03 20:57:22 +0300

    Add aflexpp

---
+++ ports/aflexpp/portfile.cmake
@@ -0,0 +1,19 @@
+vcpkg_from_git(
+    OUT_SOURCE_PATH SOURCE_PATH
+    URL git@github.com:ofstardust/flexnpp.git
+    HEAD_REF master
+    # REF ed3bf720461981c38b1abb22914aaeaefc1c9564
+)
+
+vcpkg_configure_cmake(
+    SOURCE_PATH "${SOURCE_PATH}"
+)
+
+vcpkg_install_cmake()
+
+# file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include")
+# file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/share")
+
+# vcpkg_copy_pdbs()
+
+file(INSTALL "${SOURCE_PATH}/COPYING" DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}" RENAME copyright)
