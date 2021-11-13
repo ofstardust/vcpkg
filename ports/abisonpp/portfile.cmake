828587d37b2ef1fdff4528d7ef08c7467d3b4b40
Mahder Gebremedhin <mahdual@gmail.com>
2021-11-13 21:31:11 +0100

    Update abisonpp ref.

--- ports/abisonpp/portfile.cmake
+++ ports/abisonpp/portfile.cmake
@@ -2,7 +2,7 @@ vcpkg_from_git(
     OUT_SOURCE_PATH SOURCE_PATH
     URL git@github.com:ofstardust/bisonpp.git
     HEAD_REF master
-    REF ed3bf720461981c38b1abb22914aaeaefc1c9564
+    REF bb7c53334b8d1eb2cfdef29892d7be7a2092381f
 )
 
 vcpkg_configure_cmake(
