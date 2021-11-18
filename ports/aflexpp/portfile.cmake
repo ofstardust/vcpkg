5f11c8c74e3bebe8f7a2c75b62b9061d70f713ea
Mahder Gebremedhin <mahdual@gmail.com>
2021-11-18 23:07:15 +0100

    [aflexpp] Fix MSVC build.

--- ports/aflexpp/portfile.cmake
+++ ports/aflexpp/portfile.cmake
@@ -2,7 +2,7 @@ vcpkg_from_git(
     OUT_SOURCE_PATH SOURCE_PATH
     URL git@github.com:ofstardust/flexpp.git
     HEAD_REF master
-    REF 790e6d06508dbf39001407701b0303d8916b76e6
+    REF 1ebffa4f6d2eedaf822338f40cd1ddc8ea3dbed1
 )
 
 vcpkg_configure_cmake(
